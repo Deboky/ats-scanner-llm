8cca2289a0b1e7613dd82715c298bbee59d2efa1
Deboky Saha <debokysaha1997@gmail.com>
2024-04-15 07:08:32 +0100

    added the project

---
+++ common/utils.py
@@ -0,0 +1,8 @@
+# Import all essential libraries
+import streamlit as st
+import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+from dotenv import load_dotenv
+import json
+import yaml
